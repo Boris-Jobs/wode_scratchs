0173e960b1491afc258ebf73256696194049ac1a
boris sigma <1322553126@qq.com>
2024-03-25 23:15:20 +0800

    part of stochastic gradient descent

--- linear-regression/main.py
+++ linear-regression/main.py
@@ -10,13 +10,26 @@ def synthetic_data(w, b, num_examples):
 def data_iter(batch_size, features, labels):
     num_examples = len(features)
     indices = list(range(num_examples))
-    random.shuffle()
+    random.shuffle(indices)  # 把索引进行洗牌
+    for i in range(0, num_examples, batch_size):
+        # batch_indices = indices[i: min(i + batch_size, num_examples)]
+        batch_indices = torch.tensor(indices[i: min(i + batch_size, num_examples)])  # 把batch_indices做成张量也可以。
+        yield features[batch_indices], labels[batch_indices]
+
 
 
 if __name__ == '__main__':
     true_w = torch.tensor([2, -3.4])
     true_b = 4.2
     features, labels = synthetic_data(true_w, true_b, 1000)
+    batch_size = 10
+    for X, y in data_iter(batch_size, features, labels):
+        print(X, '\n', y)
+        break
+
+    """
+
     test_tensor = torch.tensor([[[5, 3, 4], [2, 3, 4]],[[2, 3, 4],[3, 4, 5]]])
 
-    print(labels[1: 10], true_w.shape, labels.shape, test_tensor.shape)
+    print(labels[1: 10], true_w.shape, labels.shape, test_tensor.shape)    
+    """
