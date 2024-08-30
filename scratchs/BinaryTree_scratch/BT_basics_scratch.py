1692bbb38d66ed2c91d161533b5ef3a65e950e5a
boris sigma <1322553126@qq.com>
2024-08-30 11:50:33 +0800

    added the GPT-zero proj

---
+++ scratchs/BinaryTree_scratch/BT_basics_scratch.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2024-08-30 11:48:42
+
+@author: borisσ, Chairman of FrameX Inc.
+
+I hope to use AI or LLMs to help people better understand the world and humanity.
+
+We are big fans of xAI.
+
+I am recently interested in Multimodal LLMs.
+"""
+
+class TreeNode:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
